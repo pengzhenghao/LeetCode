159b4a2bf3310fd2678b8208e61b3e12e43a863a
PengZH <pengzh@sjtu.edu.cn>
2017-06-29 14:51:25 +0800

    fixed

--- 624. Maximum Distance in Arrays.py
+++ 624. Maximum Distance in Arrays.py
@@ -4,6 +4,18 @@ class Solution(object):
         :type arrays: List[List[int]]
         :rtype: int
         """
-        minimum = [x[0] for x in arrays]
-        maximum = [x[-1] for x in arrays]
-        return max(maximum)-min(minimum)
+        min,imin,amin = [arrays[0][0],0,arrays[1][0]] if arrays[0][0]<arrays[1][0] else [arrays[1][0],1,arrays[0][0]]
+        max,imax,amax = [arrays[0][-1],0,arrays[1][-1]] if arrays[0][-1]>arrays[1][-1] else [arrays[1][-1],1,arrays[0][-1]]
+        for ind in range(2,len(arrays)):
+            if arrays[ind][0]<min:
+                amin,min,imin = min,arrays[ind][0],ind
+            elif arrays[ind][0]<amin:
+                amin = arrays[ind][0]
+            if arrays[ind][-1]>max:
+                amax,max,imax = max,arrays[ind][-1],ind
+            elif arrays[ind][-1]>amax:
+                amax = arrays[ind][-1]
+        if imin==imax:
+            return max-amin if max-amin>amax-min else amax-min
+        return max-min
+                
