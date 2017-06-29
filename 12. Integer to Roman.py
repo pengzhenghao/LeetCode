f16be0e87758f0dc002873370221e556a811d646
PengZH <pengzh@sjtu.edu.cn>
2017-06-29 18:38:48 +0800

    Update 12. Integer to Roman.py

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -9,3 +9,5 @@ class Solution(object):
         X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
         I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
         return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10]
+
+        # This Code is copyed from Internet
